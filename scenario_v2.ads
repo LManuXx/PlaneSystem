3247168707b6deae4f490095c2271a839c3060a8
user <user@debian.internet.com>
2024-11-08 18:46:25 +0100

    Escenario en funcionamiento

--- scenario_v2.ads
+++ scenario_v2.ads
@@ -1,22 +1,23 @@
+
 with Ada.Real_Time; use Ada.Real_Time;
 with devicesfss_v1; use devicesfss_v1;
 
 package Scenario_V2 is
 
-    WCET_Distance: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(5);
-    WCET_Light: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(5);
-    WCET_Joystick: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(5);
-    WCET_PilotPresence: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(5);
-    WCET_PilotButton: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(5);
-    WCET_Power: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(4);
-    WCET_Speed: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(7);
-    WCET_Altitude: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(18);
-    WCET_Pitch: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(20);
-    WCET_Roll: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(18);
-    WCET_Display: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(15);
-    WCET_Alarm: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(5);
+    WCET_Distance: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(4);
+    WCET_Light: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(4);
+    WCET_Joystick: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(4);
+    WCET_PilotPresence: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(4);
+    WCET_PilotButton: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(4);
+    WCET_Power: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(2);
+    WCET_Speed: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(5);
+    WCET_Altitude: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(9);
+    WCET_Pitch: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(10);
+    WCET_Roll: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(9);
+    WCET_Display: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(7);
+    WCET_Alarm: constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds(4);
 
-    cantidad_datos_Distancia: constant := 200;
+    cantidad_datos_Distancia: constant := 100;
     type Indice_Secuencia_Distancia is mod cantidad_datos_Distancia;
     type tipo_Secuencia_Distancia is array (Indice_Secuencia_Distancia) of Distance_Samples_Type;
 
@@ -33,7 +34,7 @@ package Scenario_V2 is
               6000,6000,6000,6000,6000, 6000,6000,6000,6000,6000);  --10s
 
 
-    cantidad_datos_Light: constant := 200;
+    cantidad_datos_Light: constant := 100;
     type Indice_Secuencia_Light is mod cantidad_datos_Light;
     type tipo_Secuencia_Light is array (Indice_Secuencia_Light) of Light_Samples_Type;
 
@@ -41,16 +42,15 @@ package Scenario_V2 is
                  ( 500,500,500,500,500, 500,500,500,500,500,   --1s
                    600,600,600,600,600, 700,700,700,700,700,   --2s
                    800,800,800,800,800, 900,900,900,900,900,   --3s
-                   1000,1000,1000,1000,1000, 1100,1100,1100,1100,1100,   --4s
-                   1200,1200,1200,1200,1200, 1300,1300,1300,1300,1300,  --5s
-                   1400,1400,1400,1400,1400, 1500,1500,1500,1500,1500,    --6s
-                   1600,1600,1600,1600,1600, 1700,1700,1700,1700,1700,    --7s
-                   1800,1800,1800,1800,1800, 1900,1900,1900,1900,1900,    --8s
-                   2000,2000,2000,2000,2000, 2100,2100,2100,2100,2100,    --9s
-                   2200,2200,2200,2200,2200, 2300,2300,2300,2300,2300);
-    --10s
+                   1000,1000,1000,1000,1000, 1021,1021,1021,1021,1021,   --4s
+                   900,900,900,900,900, 950,950,950,950,950,  --5s
+                   1010,1010,1010,1010,1010, 990,990,990,990,990,    --6s
+                   700,700,700,700,700, 600,600,600,600,600,    --7s
+                   500,500,500,500,500, 400,400,400,400,400,    --8s
+                   450,450,450,450,450, 350,350,350,350,350,    --9s
+                   300,300,300,300,300, 250,250,250,250,250);   --10s
  
-    cantidad_datos_Joystick: constant := 200;
+    cantidad_datos_Joystick: constant := 100;
     type Indice_Secuencia_Joystick is mod cantidad_datos_Joystick;
     type tipo_Secuencia_Joystick is array (Indice_Secuencia_Joystick) of Joystick_Samples_Type;
 
@@ -58,13 +58,13 @@ package Scenario_V2 is
                 ((+00,+00),(+10,+10),(+20,+20),(+30,+30),(+40,+40),   
                  (+50,+50),(+60,+60),(+70,+70),(+80,+80),(+90,+90),  --1s
                  
-                 (+100,+100),(+90,+90),(+80,+80),(+70,+70),(+60,+60),   
+                 (+90,+90),(+85,+85),(+80,+80),(+70,+70),(+60,+60),   
                  (+50,+50),(+40,+40),(+30,+30),(+20,+20),(+10,+10),   --2s
 
                  (+00,+00),(+00,+00),(-10,-10),(-20,-20),(-30,-30),  
                  (-40,-40),(-50,-50),(-60,-60),(-70,-70),(-80,-80),   --3s
 
-                 (-90,-90),(-100,-100),(-90,-90),(-80,-80),(-70,-70),   
+                 (-90,-90),(-85,-85),(-85,-85),(-80,-80),(-70,-70),   
                  (-60,-60),(-50,-50),(-40,-40),(-30,-30),(-20,-20),   --4s
 
                  (-10,-10),(+00,+00),(+00,+00),(+00,+00),(+00,+00),   
@@ -73,32 +73,62 @@ package Scenario_V2 is
                  (+00,+00),(+00,+00),(+00,+00),(+00,+00),(+00,+00),   
                  (+00,+00),(+00,+00),(+00,+00),(+00,+00),(+00,+00),   --6s
 
-                 (+00,+00),(+00,+00),(+00,+00),(+00,+00),(+00,+00)    --7s
+                 (+00,+00),(+00,+00),(+00,+00),(+00,+00),(+00,+00),    --7s
                  (+00,+00),(+00,+00),(+00,+00),(+00,+00),(+00,+00),
                  
-                 (-90,-90),(-100,-100),(-90,-90),(-80,-80),(-70,-70),   --8s
+                 (-90,-90),(-90,-90),(-85,-85),(-80,-80),(-70,-70),   --8s
                  (-60,-60),(-50,-50),(-40,-40),(-30,-30),(-20,-20),
 
-                 (+100,+100),(+90,+90),(+80,+80),(+70,+70),(+60,+60),   --9s
+                 (+90,+90),(+85,+85),(+80,+80),(+70,+70),(+60,+60),   --9s
                  (+50,+50),(+40,+40),(+30,+30),(+20,+20),(+10,+10), 
 
                  (+00,+00),(+00,+00),(-10,-10),(-20,-20),(-30,-30),  --10s
                  (-40,-40),(-50,-50),(-60,-60),(-70,-70),(-80,-80));  
 
-    cantidad_datos_Power: constant := 200;
+    cantidad_datos_Power: constant := 100;
     type Indice_Secuencia_Power is mod cantidad_datos_Power;
     type tipo_Secuencia_Power is array (Indice_Secuencia_Power) of Power_Samples_Type;
 
     Power_Simulation: tipo_Secuencia_Power :=  
-                 ( 500,500,500,500,500, 600,600,600,600,600,   --1s
-                   700,700,700,700,700, 800,800,800,800,800,   --2s
-                   900,900,900,900,900, 1000,1000,1000,1000,1000,   --3s
-                   1100,1100,1100,1100,1100, 1200,1200,1200,1200,1200,  --4s 
-                   1300,1300,1300,1300,1300, 1400,1400,1400,1400,1400,  --5s
-                   1500,1500,1500,1500,1500, 1600,1600,1600,1600,1600,    --6s
-                   1700,1700,1700,1700,1700, 1800,1800,1800,1800,1800,    --7s
-                   1900,1900,1900,1900,1900, 2000,2000,2000,2000,2000,    --8s
-                   2100,2100,2100,2100,2100, 2200,2200,2200,2200,2200,    --9s
-                   2300,2300,2300,2300,2300, 2400,2400,2400,2400,2400 );  --10s
+                 ( 200, 200, 200, 200, 200, 243, 243, 243, 243, 243,    --1s
+                    287, 287, 287, 287, 287, 330, 330, 330, 330, 330,    --2s
+                    373, 373, 373, 373, 373, 417, 417, 417, 417, 417,    --3s
+                    460, 460, 460, 460, 460, 503, 503, 503, 503, 503,    --4s
+                    547, 547, 547, 547, 547, 590, 590, 590, 590, 590,    --5s
+                    633, 633, 633, 633, 633, 676, 676, 676, 676, 676,    --6s
+                    720, 720, 720, 720, 720, 763, 763, 763, 763, 763,    --7s
+                    806, 806, 806, 806, 806, 850, 850, 850, 850, 850,    --8s
+                    893, 893, 893, 893, 893, 936, 936, 936, 936, 936,    --9s
+                    980, 980, 980, 980, 980, 1023, 1023, 1023, 1023, 1023 );--10s
 
+    
+    cantidad_datos_PilotPresence: constant := 100;
+    type Indice_Secuencia_PilotPresence is mod cantidad_datos_PilotPresence;
+    type tipo_Secuencia_PilotPresence is array (Indice_Secuencia_PilotPresence) of PilotPresence_Samples_Type;
+    PilotPresence_Simulation: tipo_Secuencia_PilotPresence :=  -- 1 muestra cada 100ms.
+                 ( 1,1,1,1,1, 1,1,1,1,1,   -- 1s. 
+                   1,1,1,1,1, 1,1,1,1,1,   -- 2s.
+                   1,1,1,1,1, 1,1,1,1,1,   -- 3s.
+                   1,1,1,1,1, 1,1,0,1,0,   -- 4s. 
+                   1,1,1,1,1, 1,1,1,1,1,   -- 5s.
+                   1,1,1,1,1, 1,1,1,1,1,   -- 6s.
+                   1,1,1,1,1, 1,1,1,1,1,   -- 7s.
+                   1,1,1,1,1, 1,1,1,1,1,   -- 8s. 
+                   1,1,0,0,1, 1,1,1,1,1,   -- 9s.
+                   1,1,1,1,1, 1,1,1,1,1);   -- 10s.
+
+    cantidad_datos_PilotButton: constant := 100;
+    type Indice_Secuencia_PilotButton is mod cantidad_datos_PilotButton;
+    type tipo_Secuencia_PilotButton is array (Indice_Secuencia_PilotButton) of PilotButton_Samples_Type;
+    PilotButton_Simulation: tipo_Secuencia_PilotButton :=  -- 1 muestra cada 100ms.
+                 ( 0,0,0,0,0, 0,0,0,0,0,   -- 1s. 
+                   0,0,0,0,0, 1,1,1,0,0,   -- 2s.
+                   0,0,0,0,0, 0,0,0,0,0,   -- 3s.
+                   0,0,0,0,0, 0,0,0,0,0,   -- 4s. 
+                   1,1,1,1,0, 0,0,0,0,0,   -- 5s.
+                   0,0,0,0,0, 0,0,0,0,0,   -- 6s.
+                   0,0,0,0,0, 0,0,0,0,0,   -- 7s.
+                   0,0,0,0,0, 0,0,0,0,0,   -- 8s. 
+                   0,0,0,0,0, 0,0,0,0,0,   -- 9s.
+                   0,0,0,0,0, 0,0,0,0,0);  -- 10s. 
 end Scenario_V2;
